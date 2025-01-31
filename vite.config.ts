d3512a8627d0e2c5962e0dcdf14b8b3f3d401753
Ayham Darwish <162137522+sweco-se1c8x@users.noreply.github.com>
2025-01-31 17:36:24 +0100

    add few changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/light-control',
   server: {
-    host: true, // Listen on all addresses
-    port: 5173, // Default Vite port
-    strictPort: true,
-  }
+    port: 3000,
+  },
 })
