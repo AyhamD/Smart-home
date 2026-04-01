e091669c352657b77a2edcccb626b83bde4c7a92
sweco-se1c8x <ayham.darwish@sweco.se>
2026-04-01 08:54:07 +0200

    enable rotate

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
         theme_color: '#6366f1',
         background_color: '#0f0f23',
         display: 'standalone',
-        orientation: 'portrait',
+        orientation: 'any',
         scope: '/',
         start_url: '/',
         icons: [
